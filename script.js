02a99db17e159d22e59161af9ac39015922d675c
769k8h5byv-maker <769k8h5byv@privaterelay.appleid.com>
2026-08-19 20:28:50 -0300

    Add date display and improve task addition

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function toggleTask(checkbox) {
-    const task = checkbox.nextElementSibling;
-
-    if (checkbox.checked) {
-        task.style.textDecoration = "line-through";
-    } else {
-        task.style.textDecoration = "none";
-    }
-}
-function addTask() {
-    const input = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
-
-    const task = document.createElement("label");
-  task.innerHTML = `
-   <input type="checkbox" onchange="toggleTask(this)">
-    <span>${input.value}</span>
-`;
-
-    taskList.appendChild(task);
-
-    input.value = "";
-}
 const today = new Date();
 
 document.getElementById("date").textContent =
@@ -30,3 +7,28 @@ document.getElementById("date").textContent =
         month: "long",
         day: "numeric"
     });
+
+function toggleTask(checkbox) {
+    const task = checkbox.nextElementSibling;
+
+    if (checkbox.checked) {
+        task.style.textDecoration = "line-through";
+    } else {
+        task.style.textDecoration = "none";
+    }
+}
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+
+    const task = document.createElement("label");
+
+    task.innerHTML = `
+        <input type="checkbox" onchange="toggleTask(this)">
+        <span>${input.value}</span>
+    `;
+
+    taskList.appendChild(task);
+    input.value = "";
+}
