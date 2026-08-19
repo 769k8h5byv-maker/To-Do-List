4fc95288f8a670d0cf499b8d1268f2ae79a3bf95
769k8h5byv-maker <769k8h5byv@privaterelay.appleid.com>
2026-08-19 19:56:58 -0300

    Refactor addTask and deleteTask functions

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function toggleTask(checkbox) {
         task.style.textDecoration = "none";
     }
 }
+
 function addTask() {
     const input = document.getElementById("taskInput");
     const taskText = input.value;
@@ -14,20 +15,15 @@ function addTask() {
     if (taskText === "") {
         return;
     }
-function deleteTask(button) {
-    button.parentElement.remove();
-}
-    }
+
     const taskList = document.getElementById("taskList");
 
     const label = document.createElement("label");
 
     label.innerHTML = `
-        label.innerHTML = `
-    <input type="checkbox" onchange="toggleTask(this)">
-    <span>${taskText}</span>
-    <button onclick="deleteTask(this)">Delete</button>
-`;
+        <input type="checkbox" onchange="toggleTask(this)">
+        <span>${taskText}</span>
+        <button onclick="deleteTask(this)">Delete</button>
     `;
 
     taskList.appendChild(label);
@@ -35,4 +31,6 @@ function deleteTask(button) {
     input.value = "";
 }
 
-
+function deleteTask(button) {
+    button.parentElement.remove();
+}
